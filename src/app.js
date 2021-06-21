688e0f3599c206e07926c11c85ee03a64e019636
mabelemm <mabel.emmanuel@gmail.com>
2021-06-20 20:55:34 -0600

    Updated API key

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-function formatDatee(timestamp) {
-  let date = newDate(timestamp);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  if (minutes < 10) {
-    minutes = `0{minutes}`;
-  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -18,7 +18,7 @@ function formatDatee(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${hours} ${minutes}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -31,7 +31,7 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTM = response.data.name;
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "9401bb320e6030fd32bddc72534dad85";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "9401bb320e6030fd32bddc72534dad85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handlesubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New york");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handlesubmit);
